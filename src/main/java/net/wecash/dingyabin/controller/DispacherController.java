9c867de8a7b20bc55f93679fb3d29da55696aacc
dingyabin <1262027459@qq.com>
2017-11-12 18:34:37 +0800

    首页

--- src/main/java/net/wecash/dingyabin/controller/DispacherController.java
+++ src/main/java/net/wecash/dingyabin/controller/DispacherController.java
@@ -11,7 +11,7 @@ public class DispacherController {
 
     @RequestMapping("/")
     public String index(){
-        return "forward:/addService.html";
+        return "forward:/homepage.html";
     }
 
 
