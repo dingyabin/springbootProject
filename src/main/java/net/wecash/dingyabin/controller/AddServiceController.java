4e6aecd8b862d55454090b64e83d56eea3567627
Dingyabin <dingyabin@wecash.net>
2017-11-18 00:25:22 +0800

    del

--- src/main/java/net/wecash/dingyabin/controller/AddServiceController.java
+++ src/main/java/net/wecash/dingyabin/controller/AddServiceController.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Random;
 
 import static net.wecash.dingyabin.util.ManyUtil.assertNotNull;
 import static net.wecash.dingyabin.util.ManyUtil.isLegalId;
