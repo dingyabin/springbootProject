7c7b533f173e5479727dcf9683ff0dd266a24460
Dingyabin <dingyabin@wecash.net>
2017-11-20 18:27:00 +0800

    去掉阿波罗依赖

--- src/main/java/net/wecash/dingyabin/MainApp.java
+++ src/main/java/net/wecash/dingyabin/MainApp.java
@@ -1,9 +1,7 @@
 package net.wecash.dingyabin;
 
-import net.wecash.SimplePropertyPlaceholderConfigurer;
 import net.wecash.web.filter.LogFilter;
 import net.wecash.web.filter.TraceIdFilter;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +11,6 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.IOException;
 
 /**
  *
@@ -46,13 +41,6 @@ public class MainApp {
     @Bean
     public LogFilter logFilter() {
         return new LogFilter();
-    }
-
-    @Bean
-    public PropertyPlaceholderConfigurer propertyPlaceholder() throws IOException {
-        SimplePropertyPlaceholderConfigurer placeholderConfigurer = new SimplePropertyPlaceholderConfigurer();
-        placeholderConfigurer.setLocations(new ClassPathXmlApplicationContext().getResources("classpath*:*.properties"));
-        return placeholderConfigurer;
     }
 
 
